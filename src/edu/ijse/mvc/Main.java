bf2fc93f53eb002f196a16a55a6a964cadfad868
kaushalya999 <kaushalyaperera127@gmail.com>
2024-07-13 11:53:11 -0800

    configure save button using dto class

--- src/edu/ijse/mvc/Main.java
+++ src/edu/ijse/mvc/Main.java
@@ -4,6 +4,9 @@
  */
 package edu.ijse.mvc;
 
+import edu.ijse.mvc.view.ItemView;
+
+
 /**
  *
  * @author User
@@ -14,7 +17,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new ItemView().setVisible(true);
     }
     
 }
